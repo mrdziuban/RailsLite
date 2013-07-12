da89c1e9f06c3f8983cea49aa558038dc5bc033d
mrdziuban <mrdziuban@gmail.com>
2013-07-12 13:17:13 -0700

    Add Router#initialize, #add_route, http_methods, and #match

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -9,7 +9,8 @@ class Route
   end
 
   def matches?(req)
-    @http_method == req.request_method.downcase.to_sym
+    @http_method == req.request_method.downcase.to_sym && 
+    @pattern.match(req.path)
   end
 
   def run(req, res)
@@ -20,19 +21,26 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   def draw(&proc)
   end
 
   [:get, :post, :put, :delete].each do |http_method|
-    # add these helpers in a loop here
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   def match(req)
+    @routes.each do |route|
+      return route if route.matches?(req)
+    end
   end
 
   def run(req, res)
