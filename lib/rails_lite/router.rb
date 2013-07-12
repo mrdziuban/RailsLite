bbd4b7134e0096dae597e0a666cc5ed17d9dbc75
mrdziuban <mrdziuban@gmail.com>
2013-07-12 12:47:19 -0700

    Add Route#initialize and #matches?

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -2,9 +2,14 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   def matches?(req)
+    @http_method == req.request_method.downcase.to_sym
   end
 
   def run(req, res)
